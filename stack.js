1013982d4e292d163ba2884c75c57b961d0fab30
JeevanRajpurohit <140495760+JeevanRajpurohit@users.noreply.github.com>
2024-02-20 16:55:54 +0530

    created a stack

---
+++ stack.js
@@ -0,0 +1,47 @@
+export {stack}
+
+class stack{
+    constructor(){
+        this.size=0;
+        this.buffer=4;
+        this.stack=[];
+    }
+
+    clear(){
+        this.size=0;
+        this.stack=[];
+    }
+    isEmpty(){
+        return (this.size ===0);
+    }
+    top(){
+        return this.stack[this.size-1];
+    }
+
+    pop(){
+        if(!this.isEmpty()){
+            this.size--;
+            return this.stack.pop();
+        }else{
+            return [-1,''];
+        }
+    }
+
+    push(){
+        if(this.isEmpty()){
+            if(type===0){
+                 return this.stack.push([type,char]);
+            }else{
+                let temp=this.top();
+                if(temp[0]===type && temp[1].length < this.buffer){
+                    let top=this.pop();
+                    top[1]=char +top[1];
+                    this.stack.push(top);
+                }else{
+                    this.stack.push([type,char]);
+                }
+            }
+            this.size++;
+        }
+    }
+}
